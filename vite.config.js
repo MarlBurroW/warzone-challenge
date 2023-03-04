b88d27abdf5d20533f81643bebd96f74bd024474
Myke38 <36101817+Myke38@users.noreply.github.com>
2023-03-04 02:04:15 +0100

    Hello
    Hello

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    host: "dev.marlburrow.io",
-    port: 2456,
-  },
   meteor: {
     clientEntry: "imports/ui/main.js",
   },
